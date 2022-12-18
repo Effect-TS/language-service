ebf92b042796bc61b48067485dfea8d84d5e8c6a
Mattia Manzati <mattia.manzati@gmail.com>
2022-12-18 10:23:35 +0100

    remove useless pipe refactors

--- src/refactors/index.ts
+++ src/refactors/index.ts
@@ -1,8 +1,6 @@
-import addPipe from "@effect/language-service/refactors/addPipe"
 import asyncAwaitToGen from "@effect/language-service/refactors/asyncAwaitToGen"
 import asyncAwaitToGenTryPromise from "@effect/language-service/refactors/asyncAwaitToGenTryPromise"
 import functionToArrow from "@effect/language-service/refactors/functionToArrow"
-import removePipe from "@effect/language-service/refactors/removePipe"
 import toggleReturnTypeAnnotation from "@effect/language-service/refactors/toggleReturnTypeAnnotation"
 import toggleTypeAnnotation from "@effect/language-service/refactors/toggleTypeAnnotation"
 import wrapWithPipe from "@effect/language-service/refactors/wrapWithPipe"
@@ -10,8 +8,6 @@ import wrapWithPipe from "@effect/language-service/refactors/wrapWithPipe"
 export default {
   asyncAwaitToGen,
   asyncAwaitToGenTryPromise,
-  removePipe,
-  addPipe,
   functionToArrow,
   toggleTypeAnnotation,
   toggleReturnTypeAnnotation,
