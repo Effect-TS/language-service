e632c547371a2a1cdb53106a6383b97d1cbbd298
Mattia Manzati <mattia.manzati@gmail.com>
2023-01-22 17:17:08 +0100

    fix behavior of callable intersections (#37)

--- examples/refactors/toggleTypeAnnotation.ts
+++ examples/refactors/toggleTypeAnnotation.ts
@@ -1,4 +1,4 @@
-// 4:16,5:16,8:16,11:16,12:16,14:16,15:16,20:16,24:16
+// 4:16,5:16,8:16,11:16,12:16,14:16,15:16,20:16,24:16,38:16
 import * as T from "@effect/io/Effect"
 
 export const test1 = T.succeed
@@ -22,3 +22,17 @@ export const test6 = withOverloads
 declare const functIntersection: ((a: 1) => boolean) & ((a: 2) => string)
 
 export const test7 = functIntersection
+
+declare const intersection1: {
+    (a: 1): boolean
+    (b: 2): string
+}
+
+declare const intersection2: {
+    (a: 3): number
+    (b: 4): bigint
+}
+
+declare function intersect<A, B>(a: A, b: B): A & B
+
+export const test8 = intersect(intersection1, intersection2)
