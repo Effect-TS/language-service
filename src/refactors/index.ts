42a032c2f0c12adc41fbd2d20b7556ac9a3468cb
Mattia Manzati <mattia.manzati@gmail.com>
2023-08-13 15:19:31 +0200

    add toggle definition site lazy (#43)

--- src/refactors/index.ts
+++ src/refactors/index.ts
@@ -2,6 +2,7 @@ import asyncAwaitToGen from "@effect/language-service/refactors/asyncAwaitToGen"
 import asyncAwaitToGenTryPromise from "@effect/language-service/refactors/asyncAwaitToGenTryPromise"
 import functionToArrow from "@effect/language-service/refactors/functionToArrow"
 import pipeableToDatafirst from "@effect/language-service/refactors/pipeableToDatafirst"
+import toggleLazyConst from "@effect/language-service/refactors/toggleLazyConst"
 import toggleReturnTypeAnnotation from "@effect/language-service/refactors/toggleReturnTypeAnnotation"
 import toggleTypeAnnotation from "@effect/language-service/refactors/toggleTypeAnnotation"
 import wrapWithPipe from "@effect/language-service/refactors/wrapWithPipe"
@@ -13,5 +14,6 @@ export default {
   toggleTypeAnnotation,
   toggleReturnTypeAnnotation,
   wrapWithPipe,
-  pipeableToDatafirst
+  pipeableToDatafirst,
+  toggleLazyConst
 }
