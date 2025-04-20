ccd499dc6adcc624ad74ee616a4f98004312641e
Mattia Manzati <mattia.manzati@gmail.com>
2025-04-20 16:48:26 +0200

    begin nano

--- src/utils/Nano.ts
+++ src/utils/Nano.ts
@@ -1,51 +1,58 @@
-import * as Option from "effect/Option"
-import * as Either from "effect/Either"
 import * as Data from "effect/Data"
-import * as Gen from "effect/Utils"
+import * as Either from "effect/Either"
 import type { TypeLambda } from "effect/HKT"
-import { pipeArguments } from "effect/Pipeable"
+import * as Option from "effect/Option"
+import * as Gen from "effect/Utils"
 
-export class NanoInterruptedException extends Data.TaggedError("NanoInterruptedException")<{}>{}
-export class NanoDefectException extends Data.TaggedError("NanoDefectException")<{ value: unknown }>{}
+export class NanoInterruptedException extends Data.TaggedError("NanoInterruptedException")<{}> {}
+export class NanoDefectException
+  extends Data.TaggedError("NanoDefectException")<{ value: unknown }>
+{}
 
-export class NanoTag<R>{
-    declare "~nano.requirements": R
-    constructor(
-        readonly key: string
-    ){}
+export class NanoTag<R> {
+  declare "~nano.requirements": R
+  constructor(
+    readonly key: string
+  ) {}
 }
 
 export const Tag = <I>(identifier: string) => new NanoTag<I>(identifier)
 
 type NanoContext<R = never> = {
-    readonly _tag: "Nil"
+  readonly _tag: "Nil"
 } | {
-    readonly _tag: "Cons"
-    readonly key: string
-    readonly value: R
-    readonly next: NanoContext<R>
+  readonly _tag: "Cons"
+  readonly key: string
+  readonly value: R
+  readonly next: NanoContext<R>
 }
 
 export const contextEmpty: NanoContext<never> = { _tag: "Nil" }
-export const contextAdd = <R, I extends NanoTag<any>>(context: NanoContext<R>, tag: I, value: I["~nano.requirements"]): NanoContext<R | I> => ({
-    _tag: "Cons",
-    key: tag.key,
-    value,
-    next: context
+export const contextAdd = <R, I extends NanoTag<any>>(
+  context: NanoContext<R>,
+  tag: I,
+  value: I["~nano.requirements"]
+): NanoContext<R | I> => ({
+  _tag: "Cons",
+  key: tag.key,
+  value,
+  next: context
 })
-export const contextGet = <R, I extends NanoTag<any>>(context: NanoContext<R | I>, tag: I): Option.Option<I["~nano.requirements"]> => {
-    let current = context
-    while(current._tag !== "Nil"){
-        if(current.key === tag.key) return Option.some(current.value)
-        current = current.next
-    }
-    return Option.none()
+export const contextGet = <R, I extends NanoTag<any>>(
+  context: NanoContext<R | I>,
+  tag: I
+): Option.Option<I["~nano.requirements"]> => {
+  let current = context
+  while (current._tag !== "Nil") {
+    if (current.key === tag.key) return Option.some(current.value)
+    current = current.next
+  }
+  return Option.none()
 }
 
-
 export interface NanoIterator<T extends Nano<any, any, any>> {
-    next(...args: ReadonlyArray<any>): IteratorResult<Gen.YieldWrap<T>, T["~nano.success"]>
-  }
+  next(...args: ReadonlyArray<any>): IteratorResult<Gen.YieldWrap<T>, T["~nano.success"]>
+}
 
 /**
  * Nano is a Effect-like interface to run things.
@@ -55,49 +62,61 @@ export interface NanoIterator<T extends Nano<any, any, any>> {
  * where using full blown Effect will cause an Effect-in-Effect issue.
  * It is supposed to be sync only and not stack-safe.
  */
-export class Nano<out A, out E = never, out R = never>{
-    declare readonly "~nano.variance": {
-        _A: () => A,
-        _E: () => E,
-        _R: () => R
-    }
-    declare readonly "~nano.success": A
-    declare readonly "~nano.error": E
-    declare readonly "~nano.requirements": R
+export class Nano<out A, out E = never, out R = never> {
+  declare readonly "~nano.success": A
+  declare readonly "~nano.error": E
+  declare readonly "~nano.requirements": R
 
-    constructor(
-        public readonly run: (ctx: NanoContext<unknown>) => Either.Either<A, E | NanoInterruptedException | NanoDefectException>
-    ){}
+  constructor(
+    public readonly run: (
+      ctx: NanoContext<unknown>
+    ) => Either.Either<A, E | NanoInterruptedException | NanoDefectException>
+  ) {}
 
-    [Symbol.iterator](): NanoIterator<Nano<A, E, R>> {
-        return new Gen.SingleShotGen(new Gen.YieldWrap(this)) as any
-      }
-
-      pipe() {
-        return pipeArguments(this, arguments)
-      }
+  [Symbol.iterator](): NanoIterator<Nano<A, E, R>> {
+    return new Gen.SingleShotGen(new Gen.YieldWrap(this)) as any
+  }
 }
 
 export const run = <A, E>(fa: Nano<A, E, never>) => fa.run(contextEmpty)
 export const succeed = <A>(value: A) => new Nano(() => (Either.right(value)))
 export const fail = <E>(value: E) => new Nano(() => (Either.left(value)))
+export const sync = <A>(value: () => A) => new Nano(() => (Either.right(value())))
 
-export const service = <I extends NanoTag<any>>(tag: I) => new Nano<I["~nano.requirements"], never, I["~nano.requirements"]>((ctx) => contextGet(ctx, tag).pipe(Option.match({
-    onNone: () => Either.left(new NanoDefectException({ value: `Cannot find service ${tag.key}`})),
-    onSome: value => Either.right(value)
-})))
+export const service = <I extends NanoTag<any>>(tag: I) =>
+  new Nano<I["~nano.requirements"], never, I["~nano.requirements"]>((ctx) =>
+    contextGet(ctx, tag).pipe(Option.match({
+      onNone: () =>
+        Either.left(new NanoDefectException({ value: `Cannot find service ${tag.key}` })),
+      onSome: (value) => Either.right(value)
+    }))
+  )
+
+export const provide = <I extends NanoTag<any>>(
+  tag: I,
+  value: I["~nano.requirements"]
+) =>
+<A, E, R>(fa: Nano<A, E, R>) =>
+  new Nano<A, E, Exclude<R, I["~nano.requirements"]>>((ctx) => {
+    return fa.run(contextAdd(ctx, tag, value))
+  })
 
 export interface NanoTypeLambda extends TypeLambda {
-    readonly type: Nano<this["Target"], this["Out1"], this["Out2"]>
-  }
+  readonly type: Nano<this["Target"], this["Out1"], this["Out2"]>
+}
 
 export const gen = <Eff extends Gen.YieldWrap<Nano<any, any, any>>, AEff>(
-    ...args:[body: () => Generator<Eff, AEff, never>]
-  ) => new Nano<
-  AEff,
-  [Eff] extends [never] ? never : [Eff] extends [Gen.YieldWrap<Nano<infer _A, infer E, infer _R>>] ? E : never,
-  [Eff] extends [never] ? never : [Eff] extends [Gen.YieldWrap<Nano<infer _A, infer _E, infer R>>] ? R : never
->((ctx => {
+  ...args: [body: () => Generator<Eff, AEff, never>]
+) =>
+  new Nano<
+    AEff,
+    [Eff] extends [never] ? never
+      : [Eff] extends [Gen.YieldWrap<Nano<infer _A, infer E, infer _R>>] ? E
+      : never,
+    [Eff] extends [never] ? never
+      : [Eff] extends [Gen.YieldWrap<Nano<infer _A, infer _E, infer R>>] ? R
+      : never
+  >((ctx) => {
     const iterator = args[0]()
     let state: IteratorResult<any> = iterator.next()
     while (!state.done) {
@@ -108,8 +127,7 @@ export const gen = <Eff extends Gen.YieldWrap<Nano<any, any, any>>, AEff>(
       if (Either.isLeft(result)) {
         return result
       }
-        state = iterator.next(result.right as never)        
+      state = iterator.next(result.right as never)
     }
     return Either.right(state.value) as any
-  }))
-
+  })
--- test/Nano.test.ts
+++ test/Nano.test.ts
@@ -1,16 +1,31 @@
 import * as Nano from "@effect/language-service/utils/Nano"
 import * as Either from "effect/Either"
+import { pipe } from "effect/Function"
 
 import { describe, expect, it } from "vitest"
 
-
 describe("nano", () => {
-    it("should run gen", () => {
-        const program =Nano.gen(function*(){
-        const a = yield* Nano.succeed(1)
-        const b = yield* Nano.succeed(2)
-        return a + b
+  it("should run gen", () => {
+    const program = Nano.gen(function*() {
+      const a = yield* Nano.succeed(1)
+      const b = yield* Nano.succeed(2)
+      return a + b
     })
     expect(Nano.run(program)).toEqual(Either.right(3))
-})
+  })
+
+  it("should handle simple context", () => {
+    interface MyService {
+      value: number
+    }
+    const myServiceTag = Nano.Tag<MyService>("myServiceTag")
+    const result = pipe(
+      Nano.service(myServiceTag),
+      Nano.provide(myServiceTag, { value: 42 }),
+      Nano.run
+    )
+    expect(result).toEqual(
+      Either.right({ value: 42 })
+    )
+  })
 })
