fdd585d9b6e1f3f733f648c4c8d491a1fac1c456
Mattia Manzati <mattia.manzati@gmail.com>
2025-05-06 15:20:39 +0200

    Add support for completions for Data.TaggedClass and Data.TaggedError

--- src/completions.ts
+++ src/completions.ts
@@ -1,5 +1,11 @@
 import { contextSelfInClasses } from "./completions/contextSelfInClasses.js"
+import { effectDataClasses } from "./completions/effectDataClasses.js"
 import { effectSchemaSelfInClasses } from "./completions/effectSchemaSelfInClasses.js"
 import { effectSelfInClasses } from "./completions/effectSelfInClasses.js"
 
-export const completions = [effectSchemaSelfInClasses, effectSelfInClasses, contextSelfInClasses]
+export const completions = [
+  effectSchemaSelfInClasses,
+  effectSelfInClasses,
+  contextSelfInClasses,
+  effectDataClasses
+]
