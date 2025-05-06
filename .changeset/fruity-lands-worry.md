c325568fb6a0e6faf8524759ffe3f7ff4f21dee7
Mattia Manzati <mattia.manzati@gmail.com>
2025-05-06 15:26:14 +0200

    Add support for completions for Data.TaggedClass and Data.TaggedError (#109)

---
+++ .changeset/fruity-lands-worry.md
@@ -0,0 +1,5 @@
+---
+"@effect/language-service": patch
+---
+
+Add support for completions for Data.TaggedClass and Data.TaggedError
---
+++ src/completions/effectDataClasses.ts
@@ -0,0 +1,47 @@
+import * as Option from "effect/Option"
+import * as LSP from "../core/LSP"
+import * as Nano from "../core/Nano"
+import * as AST from "../utils/AST"
+import * as TypeScriptApi from "../utils/TypeScriptApi"
+
+export const effectDataClasses = LSP.createCompletion({
+  name: "effect/effectDataClasses",
+  apply: Nano.fn("effectDataClasses")(function*(sourceFile, position) {
+    const ts = yield* Nano.service(TypeScriptApi.TypeScriptApi)
+
+    const maybeInfos = yield* AST.parseDataForExtendsClassCompletion(sourceFile, position)
+    if (Option.isNone(maybeInfos)) return []
+    const { accessedObject, className, replacementSpan } = maybeInfos.value
+
+    // first, given the position, we go back
+    const dataName = yield* Nano.option(
+      AST.findImportedModuleIdentifierByPackageAndNameOrBarrel(
+        sourceFile,
+        "effect",
+        "Data"
+      )
+    )
+    const effectDataIdentifier = Option.match(dataName, {
+      onNone: () => "Data",
+      onSome: (_) => _.text
+    })
+
+    // ensure accessed is an identifier
+    if (effectDataIdentifier !== accessedObject.text) return []
+    const name = className.text
+
+    return [{
+      name: `TaggedError("${name}")`,
+      kind: ts.ScriptElementKind.constElement,
+      insertText: `${effectDataIdentifier}.TaggedError("${name}")<{${"${0}"}}>{}`,
+      replacementSpan,
+      isSnippet: true
+    }, {
+      name: `TaggedClass("${name}")`,
+      kind: ts.ScriptElementKind.constElement,
+      insertText: `${effectDataIdentifier}.TaggedClass("${name}")<{${"${0}"}}>{}`,
+      replacementSpan,
+      isSnippet: true
+    }] satisfies Array<LSP.CompletionEntryDefinition>
+  })
+})
