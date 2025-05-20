888909833f07bb877b2c6e7fb4895c86b01e5579
Mattia Manzati <mattia.manzati@gmail.com>
2025-05-20 14:59:13 +0200

    Improve arrow function handling for type inference

---
+++ .changeset/open-beans-enter.md
@@ -0,0 +1,5 @@
+---
+"@effect/language-service": patch
+---
+
+Improve arrow function handling for type inference
---
+++ examples/diagnostics/missingEffectContext_conciseBody.ts
@@ -0,0 +1,11 @@
+import * as Effect from "effect/Effect"
+
+class ServiceA extends Effect.Service<ServiceA>()("ServiceA", {
+    succeed: { a: 1}
+}){}
+
+declare const effectWithServices: Effect.Effect<number, never, ServiceA>
+
+//@ts-expect-error
+export const conciseBody: () => Effect.Effect<number> = () => effectWithServices
+
