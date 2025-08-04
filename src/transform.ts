810edb9994dbc8318dbb09b392433c20b3906926
Mattia Manzati <mattia.manzati@gmail.com>
2025-08-04 08:24:14 +0200

    cleanup

--- src/transform.ts
+++ src/transform.ts
@@ -12,44 +12,55 @@ import * as TypeScriptApi from "./core/TypeScriptApi"
 import * as TypeScriptUtils from "./core/TypeScriptUtils"
 import { diagnostics } from "./diagnostics"
 
-const programsChecked = new WeakSet<ts.Program>()
+const programsChecked = new WeakMap<ts.Program, Set<string>>()
 
 export default function(
   program: ts.Program,
   pluginConfig: PluginConfig,
   { addDiagnostic, ts: tsInstance }: TransformerExtras
 ) {
-  if (!programsChecked.has(program)) {
-    programsChecked.add(program)
-    const rootFileNames = program.getRootFileNames()
-    const sourceFiles = program.getSourceFiles().filter((_) => rootFileNames.indexOf(_.fileName) > -1)
+  function runDiagnostics(program: ts.Program, sourceFile: ts.SourceFile) {
+    // avoid to double-process the same file
+    const checkedFiles = programsChecked.get(program) ?? new Set<string>()
+    programsChecked.set(program, checkedFiles)
+    if (checkedFiles.has(sourceFile.fileName)) return
+    checkedFiles.add(sourceFile.fileName)
 
-    for (const sourceFile of sourceFiles) {
-      // run the diagnostics and pipe them into addDiagnostic
-      pipe(
-        LSP.getSemanticDiagnosticsWithCodeFixes(diagnostics, sourceFile),
-        TypeParser.nanoLayer,
-        TypeScriptUtils.nanoLayer,
-        Nano.provideService(TypeCheckerApi.TypeCheckerApi, program.getTypeChecker()),
-        Nano.provideService(TypeScriptApi.TypeScriptProgram, program),
-        Nano.provideService(TypeScriptApi.TypeScriptApi, tsInstance),
-        Nano.provideService(
-          LanguageServicePluginOptions.LanguageServicePluginOptions,
-          LanguageServicePluginOptions.parse(pluginConfig)
-        ),
-        Nano.run,
-        Either.map((_) => _.diagnostics),
-        Either.map(
-          Array.filter((_) =>
-            _.category === tsInstance.DiagnosticCategory.Error ||
-            _.category === tsInstance.DiagnosticCategory.Warning
-          )
-        ),
-        Either.getOrElse(() => []),
-        Array.map(addDiagnostic)
-      )
-    }
+    // run the diagnostics and pipe them into addDiagnostic
+    pipe(
+      LSP.getSemanticDiagnosticsWithCodeFixes(diagnostics, sourceFile),
+      TypeParser.nanoLayer,
+      TypeScriptUtils.nanoLayer,
+      Nano.provideService(TypeCheckerApi.TypeCheckerApi, program.getTypeChecker()),
+      Nano.provideService(TypeScriptApi.TypeScriptProgram, program),
+      Nano.provideService(TypeScriptApi.TypeScriptApi, tsInstance),
+      Nano.provideService(
+        LanguageServicePluginOptions.LanguageServicePluginOptions,
+        LanguageServicePluginOptions.parse(pluginConfig)
+      ),
+      Nano.run,
+      Either.map((_) => _.diagnostics),
+      Either.map(
+        Array.filter((_) =>
+          _.category === tsInstance.DiagnosticCategory.Error ||
+          _.category === tsInstance.DiagnosticCategory.Warning
+        )
+      ),
+      Either.getOrElse(() => []),
+      Array.map(addDiagnostic)
+    )
   }
 
-  return (_: ts.TransformationContext) => (sourceFile: ts.SourceFile) => sourceFile
+  // process root files (works for noEmit: true)
+  const rootFileNames = program.getRootFileNames()
+  const sourceFiles = program.getSourceFiles().filter((_) => rootFileNames.indexOf(_.fileName) > -1)
+  for (const sourceFile of sourceFiles) {
+    runDiagnostics(program, sourceFile)
+  }
+
+  return (_: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+    // just be sure to try process any way (for noEmit: false with non catched files)
+    runDiagnostics(program, sourceFile)
+    return sourceFile
+  }
 }
