d1fe712c691f95fede1b1315cbce31ad98ba658c
Mattia Manzati <mattia.manzati@gmail.com>
2026-03-04 15:11:18 +0100

    Fix inferred layer class name for this in effectFnOpportunity

---
+++ packages/harness-effect-v4/examples/diagnostics/effectFnOpportunity_inferredLayerThis.ts
@@ -0,0 +1,19 @@
+// @test-config { "effectFn": ["inferred-span"] }
+import { Effect, Layer, ServiceMap } from "effect"
+
+export class MyService extends ServiceMap.Service<MyService, {
+  log: (_what: string) => Effect.Effect<void>
+}>()("MyService") {
+    static layer = Layer.effect(this, Effect.gen(function*() {
+        yield* Effect.log("log")
+        return { log: (what: string) => Effect.log(what) }
+    }))
+
+    static layerSucceed = Layer.succeed(this)({
+        log: (what: string) => Effect.log(what)
+    })
+
+    static layerSync = Layer.sync(this)(() => {
+        return { log: (what: string) => Effect.log(what) }
+    })
+}
